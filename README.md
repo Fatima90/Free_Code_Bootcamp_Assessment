3f4cc2938a5432d048e242fc8fd86b29cca19ac7
Majd Eddin <almuhder@gmail.com>
2018-01-29 02:19:50 +0200

    (Add) adding the questions

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Free_Code_Bootcamp_Assessment
+
+1) Add Bootstrap to the website using Bootstrap CDN.
+2) Add a link to the styles/styles.courses
+3) Add a link to the javascript/data.js
+4) Download jQuery library to your local folder and add it to your website.
+5) Open styles/style.css file and do the following:
+    a). Change the title's class size to be 18px
+    b). You need to import Google Font and use font Lobster for the title.
+6) Remove homepage_hero image from html code.
+7) Add new div with class image_hero, you may need to define a new class called image_hero in style.css file. Add that image as background.
+8) Using Bootstrap do the following:
+    a). Create a row with three columns, each one contain the these info about the course:
+        - title
+        - image
+        - desc    
+
+5) Open javascript/data.js:
+    a).  Write a function called getCourseData that accepts number as input and return an object with the follow data (title, image, desc) from the three arrays above.
+    b).  Write a function called addDatatoArray that accept two parameters as argument arrayName and newData, will add the data to that arrays. If the data was entered correctly will return true, otherwise it will return false.
+    c). Write a function called getAllData that will return an object will all the data of the three arrays with the following keys for each item: (image, title, desc)
+    d). Using jQuery: Write a function called removeCourse will remove a course from the web page, this function will accept a number as input and will remove that course if it was shown in the webpage and return the course with index X was removed successfully, otherwise it will return sorry we didn't find that course.
---
+++ javascript/data.js
@@ -0,0 +1,82 @@
+var courses = {};
+
+var images = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    'image4.jpg',
+    'image5.jpg',
+    'image6.jpg',
+    'image7.jpg',
+    'image8.jpg',
+    'image9.jpg',
+    'image10.jpg',
+];
+
+var titles = [
+    'Cloud GURU',
+    'Adobe Photoshop',
+    '3D Max',
+    'Unity 3D Game Engine',
+    'Mobile App Development',
+    'Adobe Illustrator',
+    'Angular',
+    'UX/UI Design',
+    'Complete Python Course',
+    'Complete Web Developer Course',
+];
+
+var descs = [
+    'Become Cloud GURU',
+    'Master Graphic Design',
+    'Learn How to Create 3D Objects',
+    'Learn How to Build Your Own Game with Unity 3D ',
+    'Build Your Next Mobile App',
+    'Learn How to Draw Vectors with Adobe Illustrator',
+    'Learn Front End Development with Angular ',
+    'Learn The Basics of UX/UI Design',
+    'Master Python Language',
+    'Become Web Developer This Month',
+];
+
+// 1). Write a function called getCourseData that accepts number as input and return an object with the follow data (title, image, desc) from the three arrays above.
+function getCourseData(index) {
+
+}
+// For Examlpe:
+getCourseData(0); // ==>
+// obj = {
+//     title: 'Cloud GURU',
+//     image: 'image1.jpg',
+//     desc: 'Become Cloud GURU',
+//   };
+
+
+// 2). Write a function called addDatatoArray that accept two parameters as argument arrayName and newData, will add the data to that arrays. If the data was entered correctly will return true, otherwise it will return false.
+function addDatatoArray(array, data) {
+
+}
+// For Examlpe:
+addDatatoArray('images', 'images11.jpg'); // true
+
+
+
+// 3). Write a function called getAllData that will return an object will all the data of the three arrays with the following keys for each item: (image, title, desc)
+
+function getAllData() {
+  ob = {};
+  return ob;
+}
+
+
+//-------------------------------//
+//          Using jQuery
+//-------------------------------//
+// 3. Write a function called removeCourse will remove a course from the web page, this function will accept a number as input and will remove that course if it was shown in the webpage and return the course with index X was removed successfully, otherwise it will return sorry we didn't find that course.
+
+function removeCourse(index) {
+
+}
+
+removeCourse(5); // "Course with index 5 was removed successfully "
+removeCourse(100); // "Sorry! We didn't find that course."
